6461106df89c41d13474cce57ffb980fac0b042c
danu53 <danu53555@gmail.com>
2025-06-15 11:33:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const chalk = require('chalk')
 
 global.sessionID = process.env.SESSIONID || "ᴘᴜᴛ ʏᴏᴜʀ sᴇssɪᴏɴ ɪᴅ ʜᴇʀᴇ."
 global.botname = process.env.BOTNAME || "KAVI-X MD"
-global.ownernumber = process.env.OWNERNUMBER || "94702128378"
-global.ownername = process.env.OWNERNAME || "Cyber Kavi"
+global.ownernumber = process.env.OWNERNUMBER || "94766854478"
+global.ownername = process.env.OWNERNAME || "Cyber spec"
 global.websitex = process.env.WEBSITEX || "https://kavi-x-bot-login.up.railway.app/"
 global.wagc = process.env.WAGC || "https://chat.whatsapp.com/CG9f0paHJzwDImXiydfuht"
 global.botscript = process.env.BOTSCRIPT || "https://github.com/KaviDeveloperSe/KAVI-X-BOT"
@@ -13,7 +13,7 @@ global.author = process.env.AUTHOR || "Cyber Kavi"
 global.creator = process.env.CREATOR || "94702128378@s.whatsapp.net"
 global.botprefix = process.env.BOTPREFIX || "."
 global.restart = process.env.RESTART || true
-global.mongoDB = process.env.MONGODB_URI || "ᴘᴜᴛ ʏᴏᴜʀ ᴍᴏɴɢᴏᴅʙ ᴜʀɪ ʜᴇʀᴇ."
+global.mongoDB = process.env.MONGODB_URI || "mongodb+srv://denit778:OsjM0zHvEh1B5YnL@cluster0.35igjbn.mongodb.net/"
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
